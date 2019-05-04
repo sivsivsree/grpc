5ee0ac5cccbcf2cd961791730340074471614f19
siv <sivsivsree@gmail.com>
2019-05-04 16:25:47 +0400

    Created Protobuff Files.

---
+++ todo/cmd/server/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/sivsivsree/grpc/todo"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+)
+
+type taskServer struct {
+}
+
+func (ts taskServer) List(context.Context, *todo.Void) (*todo.TaskList, error) {
+
+}
+
+func main() {
+	srv := grpc.NewServer()
+	var tasks taskServer
+	todo.RegisterTasksServer(srv, tasks)
+	l, err := net.Listen("tcp", ":8888")
+
+	if err != nil {
+		fmt.Errorf("TCP server failed. %v", err)
+	}
+	log.Fatal(srv.Serve(l))
+}
+
+const dbPath = "mydb.pb"
+
+func add(text string) error {
+
+	task := &todo.Task{
+		Text: text,
+		Done: false,
+	}
+
+	b, err := proto.Marshal(task)
+
+	if err != nil {
+		fmt.Errorf("Unable to encode protocol buffer")
+	}
+
+	f, err := os.OpenFile(dbPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Errorf("Database cannot be found %s : %v", dbPath, err)
+	}
+
+	_, werr := f.Write(b)
+
+	if werr != nil {
+		fmt.Errorf("coudnt write to database %s : %v", dbPath, err)
+	}
+
+	f.Close()
+
+	return nil
+}
+
+func list() error {
+	return nil
+}
